648d859cb1e24a5d4125cc76648409f28b6b2739
ZihanType <ZihanType@protonmail.com>
2022-03-11 12:29:19 +0800

    Add tokei(12.1.2)

---
+++ Casks/tokei-bin.rb
@@ -0,0 +1,11 @@
+cask "tokei-bin" do
+  version "12.1.2"
+  sha256 "2af8abb6a08b0513f9d16ca2c7cd37949b858d2a3e3227be8cc412b3b4937d5b"
+
+  url "https://github.com/XAMPPRocky/tokei/releases/download/v#{version}/tokei-x86_64-apple-darwin.tar.gz"
+  name "tokei-bin"
+  desc "Count your code, quickly."
+  homepage "https://github.com/XAMPPRocky/tokei"
+
+  binary "GNUSparseFile.0/tokei"
+end
