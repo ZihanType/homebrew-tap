a3ae57666f3b2b2be46066e5e51ee1f09038d600
ZihanType <ZihanType@protonmail.com>
2022-03-22 13:50:34 +0800

    Fix orange replace orange to orange-search

--- Casks/orange-search.rb
+++ Casks/orange-search.rb
@@ -3,7 +3,7 @@ cask "orange-search" do
   sha256 "4d2d3f9c6bde0916c2e4a139a71cc782365bc24a62de22b310d361ec5e8bbc33"
 
   url "https://github.com/naaive/orange/releases/download/orange-v#{version}/Orange_#{version}_x64.dmg"
-  name "orange"
+  name "orange-search"
   desc "Cross-platform local file search engine."
   homepage "https://github.com/naaive/orange"
 
