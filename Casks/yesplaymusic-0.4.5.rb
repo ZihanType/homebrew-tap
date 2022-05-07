ee2a0dfaf9a1dcd3b69068bae720d14d3d84cb87
ZihanType <ZihanType@protonmail.com>
2022-05-07 14:22:33 +0800

    Update yesplaymusic-0.4.5

--- Casks/yesplaymusic-0.4.5.rb
+++ Casks/yesplaymusic-0.4.5.rb
@@ -1,5 +1,5 @@
 cask "yesplaymusic-0.4.5" do
-  arch = Hardware::CPU.intel? ? "" : "-arm64"
+  arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
   version "0.4.5"
 
@@ -9,7 +9,7 @@ cask "yesplaymusic-0.4.5" do
     sha256 "428d37ac64bfa81e9273522e9d4ec5f35589fedfa6591ed946c2982bdecb1e29"
   end
 
-  url "https://github.com/qier222/YesPlayMusic/releases/download/v#{version}/YesPlayMusic-mac-#{version}#{arch}.dmg"
+  url "https://github.com/qier222/YesPlayMusic/releases/download/v#{version}/YesPlayMusic-mac-#{version}-#{arch}.dmg"
   name "YesPlayMusic"
   desc "Third-party NetEase cloud player"
   homepage "https://github.com/qier222/YesPlayMusic"
