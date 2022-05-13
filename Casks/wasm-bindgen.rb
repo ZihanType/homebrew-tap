1ef406ec9c6d0236003ba8aec41a0cb81edff3f6
ZihanType <ZihanType@protonmail.com>
2022-05-13 14:55:07 +0800

    Add wasm-bindgen 0.2.80

---
+++ Casks/wasm-bindgen.rb
@@ -0,0 +1,16 @@
+cask "wasm-bindgen-bin" do
+  version "0.2.80"
+  sha256 "60ac68ac15a4a1991551450954c0300850e8a69d91a311035132e3979a75b40c"
+
+  url "https://github.com/rustwasm/wasm-bindgen/releases/download/#{version}/wasm-bindgen-#{version}-x86_64-apple-darwin.tar.gz"
+  name "wasm-bindgen-bin"
+  desc "Facilitating high-level interactions between Wasm modules and JavaScript"
+  homepage "https://github.com/rustwasm/wasm-bindgen"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  binary "wasm-bindgen-#{version}-x86_64-apple-darwin/wasm-pack"
+end
