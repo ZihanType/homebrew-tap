13721f4c95d7c6a8fb4cd7140ed71c8b66c16e45
ZihanType <ZihanType@protonmail.com>
2022-05-13 14:42:05 +0800

    Add wasm-pack

---
+++ Casks/wasm-pack-bin.rb
@@ -0,0 +1,16 @@
+cask "wasm-pack-bin" do
+  version "0.10.2"
+  sha256 "6d714c86a6aae1fcde5c6829f2fc17e2aa1588cb35788e1c693e9d8e89cb0430"
+  
+  url "https://github.com/rustwasm/wasm-pack/releases/download/v#{version}/wasm-pack-v#{version}-x86_64-apple-darwin.tar.gz"
+  name "wasm-pack-bin"
+  desc "Your favorite rust -> wasm workflow tool!"
+  homepage "https://github.com/rustwasm/wasm-pack"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  binary "wasm-pack"
+end
