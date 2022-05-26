5e0642e8abee1556dd85561295de4a999325d96b
ZihanType <ZihanType@protonmail.com>
2022-05-26 18:02:50 +0800

    Update starship version

--- Casks/starship-bin.rb
+++ Casks/starship-bin.rb
@@ -1,6 +1,6 @@
 cask "starship-bin" do
-  version "1.6.3"
-  sha256 "e1fa73c50852949c1ad304c9125f9097ac5de4dcf249bd90fcdcf016494f6f12"
+  version "1.7.1"
+  sha256 "514e9e1a64fc2b478523af9c9942a484480f31ca7d43a1bf98c2de579a7d6261"
 
   url "https://github.com/starship/starship/releases/download/v#{version}/starship-x86_64-apple-darwin.tar.gz"
   name "starship-bin"
