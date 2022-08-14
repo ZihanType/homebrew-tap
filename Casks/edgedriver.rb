a75920b8a00d2549f663855cf82086cc9bb3bd45
ZihanType <ZihanType@protonmail.com>
2022-08-14 21:44:49 +0800

    Update EdgeDriver version to 104.0.1293.54

--- Casks/edgedriver.rb
+++ Casks/edgedriver.rb
@@ -1,6 +1,6 @@
 cask "edgedriver" do
-  version "104.0.1293.47"
-  sha256 "0a0d8cfcfcbb77bc7524c30eed5943065dff9274035d0633c3c6bb3ac05f4d5b"
+  version "104.0.1293.54"
+  sha256 "8286732d150568ce21fbfc35f6045b594b4e7e5f1e49e71831be07c20bff3433"
   linkid = Hardware::CPU.intel? ? "2069148" : "2093504"
 
   url "https://msedgedriver.azureedge.net/#{version}/edgedriver_mac64.zip"
