9d2dad51a4db0a34c5810a6d77a2f92f443f8477
ZihanType <ZihanType@protonmail.com>
2022-08-26 16:23:58 +0800

    Add intellij idea cli

---
+++ Casks/intellij-idea-cli.rb
@@ -0,0 +1,24 @@
+cask "intellij-idea-cli" do
+  version "1.0.0"
+  sha256 :no_check
+
+  url "https://www.jetbrains.com/help/idea/working-with-the-ide-features-from-command-line.html"
+  name "intellij-idea-cli"
+  desc "IntelliJ IDEA CLI"
+
+  idea = "#{staged_path}/idea"
+  livecheck do
+    skip "no version"
+  end
+
+  depends_on cask: "intellij-idea"
+
+  binary idea
+
+  preflight do
+    File.write idea, <<~EOS
+      #!/bin/sh
+      open -na "IntelliJ IDEA.app" --args "$@"
+    EOS
+  end
+end
