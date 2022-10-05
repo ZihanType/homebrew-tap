5ff930f49672794b77da9526e7d7716a713590af
ZihanType <ZihanType@protonmail.com>
2022-10-05 14:57:07 +0800

    Update pnpm version to 7.13.1

--- Casks/pnpm-bin.rb
+++ Casks/pnpm-bin.rb
@@ -1,6 +1,6 @@
 cask "pnpm-bin" do
-  version "7.13.0"
-  sha256 "5197810f2f0bb2bc45853a61545a08f3d0783dffcc99b34c163d27f17dc0722f"
+  version "7.13.1"
+  sha256 "d663f3a18c13e3d33a58bdc69ca214bce26519f3f2f6a7e2c0a3f8d922c483ba"
 
   url "https://github.com/pnpm/pnpm/releases/download/v#{version}/pnpm-macos-x64"
   name "pnpm-bin"
