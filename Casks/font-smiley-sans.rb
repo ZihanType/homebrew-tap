8a3637811e213b1095a8d5a7cc3a7ef96dc840a3
ZihanType <ZihanType@protonmail.com>
2022-11-24 11:13:55 +0800

    Add `Smiley Sans`

---
+++ Casks/font-smiley-sans.rb
@@ -0,0 +1,17 @@
+cask "font-smiley-sans" do
+  version "1.0.0"
+  sha256 "8293c9b9fd44c9ee6b3fab696862445059e4d98b341a148d4544f91d01363fc1"
+
+  url "https://github.com/atelier-anchor/smiley-sans/releases/download/v#{version}/smiley-sans-v#{version}.zip"
+  name "Smiley Sans"
+  name "得意黑"
+  desc "一款在人文观感和几何特征中寻找平衡的中文黑体"
+  homepage "https://github.com/atelier-anchor/smiley-sans"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  font "smiley-sans-v#{version}/SmileySans-Oblique.ttf"
+end
