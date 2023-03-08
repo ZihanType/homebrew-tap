23601453281155d7ba79101068eef7cd4046acd1
ZihanType <ZihanType@protonmail.com>
2023-03-08 13:41:16 +0800

    pnpm-bin 7.29.1

--- Casks/pnpm-bin.rb
+++ Casks/pnpm-bin.rb
@@ -1,6 +1,6 @@
 cask "pnpm-bin" do
-  version "7.29.0"
-  sha256 "993569d264add28a4ea459212624aaea1c926e5856613969517cc9e6c7cb7b17"
+  version "7.29.1"
+  sha256 "f52131913bd15b5aec7039bbb845036c37c9d2e8fd7f404c721847b261150341"
 
   url "https://github.com/pnpm/pnpm/releases/download/v#{version}/pnpm-macos-x64"
   name "pnpm-bin"
