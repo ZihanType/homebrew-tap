2e2f0aafbac95dcf7a0766b97bc09876518b3e06
ZihanType <ZihanType@protonmail.com>
2023-03-14 09:29:10 +0800

    Update cargo-nextest-bin 0.9.50

--- Casks/cargo-nextest-bin.rb
+++ Casks/cargo-nextest-bin.rb
@@ -1,6 +1,6 @@
 cask "cargo-nextest-bin" do
-  version "0.9.49"
-  sha256 "171bc696474667208587c74d9acce0250f3b2eac1faad39458e828a65c3f1c93"
+  version "0.9.50"
+  sha256 "77405db34f1191cc6979b3e291e7aaf587a40ee231cbccbe1885fd89e31ee577"
 
   url "https://github.com/nextest-rs/nextest/releases/download/cargo-nextest-#{version}/cargo-nextest-#{version}-universal-apple-darwin.tar.gz"
   name "cargo-nextest-bin"
