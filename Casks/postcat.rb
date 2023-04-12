db5c09132708bd66ec11a3ff14592a69cc097a70
ZihanType <ZihanType@protonmail.com>
2023-04-12 16:45:18 +0800

    Update postcat 0.5.1

--- Casks/postcat.rb
+++ Casks/postcat.rb
@@ -1,6 +1,6 @@
 cask "postcat" do
-  version "0.4.0"
-  sha256 "1ff8431ba516302de22bd6764c0d0c900a7eccb42883419e7e0273368f4c1ba5"
+  version "0.5.1"
+  sha256 "36f372bfc82a37cc9fb5e09c095de7bbc0b959769936a6faea741a1656dc6290"
 
   url "https://github.com/Postcatlab/postcat/releases/download/v#{version}/Postcat-#{version}-mac.zip"
   name "Postcat"
