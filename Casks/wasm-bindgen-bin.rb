859bb8be843508ebb6ba89023773cf9bc0fa335f
ZihanType <ZihanType@protonmail.com>
2023-05-10 09:44:44 +0800

    Update wasm-bindgen-bin 0.2.85

--- Casks/wasm-bindgen-bin.rb
+++ Casks/wasm-bindgen-bin.rb
@@ -1,6 +1,6 @@
 cask "wasm-bindgen-bin" do
-  version "0.2.84"
-  sha256 "554b920b01776ce0fcf74a6332f33c15640a0e8fcee3c921311545236a2f01f8"
+  version "0.2.85"
+  sha256 "93ad6a22d21a8f3ef1a1f7fabeea794c654978b9122e503b6f884a0b6f9d8aa5"
 
   url "https://github.com/rustwasm/wasm-bindgen/releases/download/#{version}/wasm-bindgen-#{version}-x86_64-apple-darwin.tar.gz"
   name "wasm-bindgen-bin"
