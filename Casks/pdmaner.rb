e69c6ed0eedf9b705c9d56767e86d55385f33483
ZihanType <ZihanType@protonmail.com>
2023-06-05 10:46:38 +0800

    Update pdmaner 4.6.0

--- Casks/pdmaner.rb
+++ Casks/pdmaner.rb
@@ -1,8 +1,8 @@
 cask "pdmaner" do
-  version "4.5.1"
-  sha256 "09b551fba220c76dd5e707c149662900fe8393e4e5cdd00b650c7cfedb59827f"
+  version "4.6.0"
+  sha256 "bf7ea03f5f6f2e0d07f3663c52afed6ef12da8f2b23f04ffb4e147688b9657be"
 
-  url "https://github.com/ZihanType/homebrew-tap/releases/download/pdmaner-v#{version}/PDManer-mac_x64_v#{version}.dmg"
+  url "https://github.com/ZihanType/homebrew-tap/releases/download/pdmaner-v#{version}/PDManer-mac_v#{version}.dmg"
   name "PDManer"
   desc "PDManer元数建模"
   homepage "https://gitee.com/robergroup/pdmaner"
