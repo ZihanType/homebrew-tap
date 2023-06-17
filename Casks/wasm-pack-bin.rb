7da4e3d84a2135016f7d84fec55039d67ac565f3
ZihanType <ZihanType@protonmail.com>
2023-06-17 20:03:06 +0800

    Update wasm-pack-bin 0.12.0

--- Casks/wasm-pack-bin.rb
+++ Casks/wasm-pack-bin.rb
@@ -1,6 +1,6 @@
 cask "wasm-pack-bin" do
-  version "0.11.1"
-  sha256 "6925ede1db571ceca80f2d48533cd9983f152731d95b794de6826121e5cd603f"
+  version "0.12.0"
+  sha256 "a2fb0a8ab4b5ae1f7dd459af4634d062d7ca33200b1fe6fb527c3f3797b28fe3"
 
   url "https://github.com/rustwasm/wasm-pack/releases/download/v#{version}/wasm-pack-v#{version}-x86_64-apple-darwin.tar.gz"
   name "wasm-pack-bin"
