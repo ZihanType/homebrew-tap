207bc77245a3c1f1d8ea8ad759bf3c955be5d4b8
ZihanType <ZihanType@protonmail.com>
2023-06-28 09:45:05 +0800

    Update sccache-bin 0.5.4

--- Casks/sccache-bin.rb
+++ Casks/sccache-bin.rb
@@ -1,6 +1,6 @@
 cask "sccache-bin" do
-  version "0.5.3"
-  sha256 "31e7b81731220f892357c0de88bd85d9059d24a803ca754ec6d043a6d4c54540"
+  version "0.5.4"
+  sha256 "b404cf83dd20c2d7cc9f08ccea1fc593442d87f4112983ba44d9bcefebf15016"
 
   url "https://github.com/mozilla/sccache/releases/download/v#{version}/sccache-v#{version}-x86_64-apple-darwin.tar.gz"
   name "sccache-bin"
