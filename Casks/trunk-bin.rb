48212e2c1e1ce4f0edca76bf0d52d2f0250a2fc7
ZihanType <ZihanType@protonmail.com>
2023-06-29 09:38:45 +0800

    Update trunk-bin 0.17.1

--- Casks/trunk-bin.rb
+++ Casks/trunk-bin.rb
@@ -1,6 +1,6 @@
 cask "trunk-bin" do
-  version "0.17.0"
-  sha256 "f0b43c7591156a8656773c2dbfeefc6efe76e8558cefb324aed3d0b8af625258"
+  version "0.17.1"
+  sha256 "bd108fb45b8b95e2feeb3fb13735b337690b3f67105d8a5d7d912502942f87ab"
 
   url "https://github.com/thedodd/trunk/releases/download/v#{version}/trunk-x86_64-apple-darwin.tar.gz"
   name "trunk-bin"
