189e34d4175db84c5e5fc92493e5040be4458cfe
ZihanType <ZihanType@protonmail.com>
2023-07-02 21:07:29 +0800

    Update erdtree-bin 3.1.2

--- Casks/erdtree-bin.rb
+++ Casks/erdtree-bin.rb
@@ -1,6 +1,6 @@
 cask "erdtree-bin" do
-  version "3.1.1"
-  sha256 "70041c091000f1eee0eec4e8e58dff1649c8c62973a95d4b01acaea93937df80"
+  version "3.1.2"
+  sha256 "ba817e64b09e2f2505c8ce71df941aa5d73046d5bebdff83b656541cdbead688"
 
   url "https://github.com/solidiquis/erdtree/releases/download/v#{version}/erd-v#{version}-x86_64-apple-darwin.tar.gz"
   name "erdtree-bin"
