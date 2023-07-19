fc85fe679b7dd749b345a9523534f4c707306c89
ZihanType <ZihanType@protonmail.com>
2023-07-19 09:13:47 +0800

    Update rye-bin 0.11.0

--- Casks/rye-bin.rb
+++ Casks/rye-bin.rb
@@ -1,6 +1,6 @@
 cask "rye-bin" do
-  version "0.10.0"
-  sha256 "fcd5c343ef930846a07364f5e475f2a780151829ee043830b6648d86e578dbd1"
+  version "0.11.0"
+  sha256 "0e0f76b43b02c04f2159df75a4e0e39e6642d50efed4a4f0d6d7d3ca42d8eb34"
 
   url "https://github.com/mitsuhiko/rye/releases/download/#{version}/rye-x86_64-macos.gz"
   name "rye-bin"
