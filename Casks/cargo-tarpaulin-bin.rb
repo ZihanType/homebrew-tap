46359072eba78084af610de3128f55e9925eb7f1
ZihanType <ZihanType@protonmail.com>
2023-07-28 09:32:01 +0800

    Update cargo-tarpaulin-bin 0.26.1

--- Casks/cargo-tarpaulin-bin.rb
+++ Casks/cargo-tarpaulin-bin.rb
@@ -1,6 +1,6 @@
 cask "cargo-tarpaulin-bin" do
-  version "0.26.0"
-  sha256 "e4640fcdc031dd02b020ed4ddb4aafd2d073299c575d798b638612028b37f0fe"
+  version "0.26.1"
+  sha256 "6000aa742b2ec44d4c9a2ab5873847703b53597f0ff296c4ae1096c572ce5565"
 
   url "https://github.com/xd009642/tarpaulin/releases/download/#{version}/cargo-tarpaulin-x86_64-apple-darwin.tar.gz"
   name "cargo-tarpaulin-bin"
