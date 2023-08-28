f5d6010474f24a86e06f26d6b870943ed0319355
ZihanType <ZihanType@protonmail.com>
2023-08-28 15:55:42 +0800

    Update lsd-bin 1.0.0

--- Casks/lsd-bin.rb
+++ Casks/lsd-bin.rb
@@ -1,6 +1,6 @@
 cask "lsd-bin" do
-  version "0.23.1"
-  sha256 "7621f9d8874fe2ec0bd103e0e180db4d3f3dcf42395a01f9829ac9e3feccbc15"
+  version "1.0.0"
+  sha256 "ffff5b77bd53b5d90097da38c9bbbfba7020bc217bd027c788a065df5e89a92f"
 
   url "https://github.com/Peltoche/lsd/releases/download/#{version}/lsd-#{version}-x86_64-apple-darwin.tar.gz"
   name "lsd-bin"
