83fd6def3587fe1ecc841e2414f27387cf3ff8ee
ZihanType <ZihanType@protonmail.com>
2023-08-28 15:59:20 +0800

    Fix lsd-bin url

--- Casks/lsd-bin.rb
+++ Casks/lsd-bin.rb
@@ -2,10 +2,10 @@ cask "lsd-bin" do
   version "1.0.0"
   sha256 "ffff5b77bd53b5d90097da38c9bbbfba7020bc217bd027c788a065df5e89a92f"
 
-  url "https://github.com/Peltoche/lsd/releases/download/#{version}/lsd-#{version}-x86_64-apple-darwin.tar.gz"
+  url "https://github.com/lsd-rs/lsd/releases/download/v#{version}/lsd-v#{version}-x86_64-apple-darwin.tar.gz"
   name "lsd-bin"
   desc "The next gen ls command"
-  homepage "https://github.com/Peltoche/lsd"
+  homepage "https://github.com/lsd-rs/lsd"
 
   livecheck do
     url :url
