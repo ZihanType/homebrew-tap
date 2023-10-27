9c237ac0fcd34f0b91df1d803a7af862918d9535
ZihanType <ZihanType@protonmail.com>
2023-10-27 09:57:25 +0800

    Add hyperfine-bin 1.18.0

---
+++ Casks/hyperfine-bin.rb
@@ -0,0 +1,28 @@
+cask "hyperfine-bin" do
+  version "1.18.0"
+  sha256 "f421263f160f4b4967d0a1420b06a48cb60e429c610096f99bb433dd719685c6"
+
+  url "https://github.com/sharkdp/hyperfine/releases/download/v#{version}/hyperfine-v#{version}-x86_64-apple-darwin.tar.gz"
+  name "hyperfine-bin"
+  desc "Command-line benchmarking tool"
+  homepage "https://github.com/sharkdp/hyperfine"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  binary "hyperfine-v#{version}-x86_64-apple-darwin/hyperfine"
+  binary "hyperfine-v#{version}-x86_64-apple-darwin/autocomplete/hyperfine.bash",
+         target: "#{HOMEBREW_PREFIX}/etc/bash_completion.d/hyperfine"
+  binary "hyperfine-v#{version}-x86_64-apple-darwin/autocomplete/_hyperfine",
+         target: "#{HOMEBREW_PREFIX}/share/zsh/site-functions/_hyperfine"
+  binary "hyperfine-v#{version}-x86_64-apple-darwin/autocomplete/hyperfine.fish",
+         target: "#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d/hyperfine.fish"
+
+  uninstall delete: [
+    "#{HOMEBREW_PREFIX}/etc/bash_completion.d/hyperfine",
+    "#{HOMEBREW_PREFIX}/share/zsh/site-functions/_hyperfine",
+    "#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d/hyperfine.fish",
+  ]
+end
