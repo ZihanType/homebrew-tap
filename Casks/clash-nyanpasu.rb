2ad8a9c2157ee62f1cbd5c29a24fd177a1e9fe67
ZihanType <ZihanType@protonmail.com>
2023-12-25 13:47:23 +0800

    Update clash-nyanpasu 1.4.2

--- Casks/clash-nyanpasu.rb
+++ Casks/clash-nyanpasu.rb
@@ -1,6 +1,6 @@
 cask "clash-nyanpasu" do
-  version "1.4.1"
-  sha256 "c4c28b1d5cc798f8cfca02f6408c13710022b43fe3205f5950a094c2e5bb145f"
+  version "1.4.2"
+  sha256 "8d99046c64dec68acd219fbdc529a019e2368e75ca86b2dec8d51206ac932803"
 
   url "https://github.com/keiko233/clash-nyanpasu/releases/download/v#{version}/Clash.Nyanpasu_#{version}_x64.dmg"
   name "Clash Nyanpasu"
