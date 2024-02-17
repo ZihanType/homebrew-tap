3c4a4fdcc3ef33e9924ca4b015a17bd9b015fa29
ZihanType <ZihanType@protonmail.com>
2024-02-17 22:10:26 +0800

    Update bun-bin 1.0.27

--- Casks/bun-bin.rb
+++ Casks/bun-bin.rb
@@ -1,6 +1,6 @@
 cask "bun-bin" do
-  version "1.0.26"
-  sha256 "9a52aca5c1697985c9b883580ef0a80c4b8d1c90cc703aa780f3480690f7f53e" # bun-darwin-x64.zip
+  version "1.0.27"
+  sha256 "7e263c729cbc44418e6844d27142696cdd074c94f48007e349f18b48305e52cf" # bun-darwin-x64.zip
 
   url "https://github.com/oven-sh/bun/releases/download/bun-v#{version}/bun-darwin-x64.zip"
   name "bun-bin"
