859c5cedda02baf961b2bf1225f3eb0605a2d216
ZihanType <ZihanType@protonmail.com>
2024-02-22 23:35:09 +0800

    Update cargo-fuzz-bin 0.12.0

--- Casks/cargo-fuzz-bin.rb
+++ Casks/cargo-fuzz-bin.rb
@@ -1,6 +1,6 @@
 cask "cargo-fuzz-bin" do
-  version "0.11.4"
-  sha256 "0cf9c6d7a8b3de49eb8a518951aa8a7ad0733244294d075af5bc6486ac5bb2f2"
+  version "0.12.0"
+  sha256 "f28dece7bceefe9069632b677111f157b5c62c7b006c665d191bf562d9b56f8e"
 
   url "https://github.com/rust-fuzz/cargo-fuzz/releases/download/#{version}/cargo-fuzz-#{version}-x86_64-apple-darwin.tar.gz"
   name "cargo-fuzz-bin"
