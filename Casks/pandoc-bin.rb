de703cea62ea8bcf87fec75578ea1ff984963a47
ZihanType <ZihanType@protonmail.com>
2024-03-01 15:42:17 +0800

    Update pandoc-bin 3.1.12.2

--- Casks/pandoc-bin.rb
+++ Casks/pandoc-bin.rb
@@ -1,6 +1,6 @@
 cask "pandoc-bin" do
-  version "3.1.12.1"
-  sha256 "d23cf1d8e6f91c3389f7cd0b95c0d94f699ffcfc2f12355e60b163a096bf4e0e"
+  version "3.1.12.2"
+  sha256 "f693cc59619053da7a6606ee8ab812664ee033f5efca88da92077c96066fa37b"
 
   url "https://github.com/jgm/pandoc/releases/download/#{version}/pandoc-#{version}-x86_64-macOS.zip"
   name "pandoc-bin"
