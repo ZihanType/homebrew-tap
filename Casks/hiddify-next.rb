f913f5743f49299aa5903a202d8addb2b977580e
ZihanType <ZihanType@protonmail.com>
2024-04-28 20:21:30 +0800

    Update hiddify-next 1.1.1

--- Casks/hiddify-next.rb
+++ Casks/hiddify-next.rb
@@ -1,20 +1,18 @@
 cask "hiddify-next" do
-  version "0.14.20"
-  sha256 "9b566df627528355f76bfd18fd255a51f61951cc5369cfd9c2e06d2b1e30d527"
+  version "1.1.1"
+  sha256 "ef238c7b02955fffdb47421fe371a30f7c12839a17a83c3dee3c3f95a5f67379"
 
-  url "https://github.com/hiddify/hiddify-next/releases/download/v#{version}/hiddify-macos-universal.zip"
+  url "https://github.com/hiddify/hiddify-next/releases/download/v#{version}/Hiddify-MacOS.dmg"
   name "Hiddify-Next"
   desc "Multi-platform auto-proxy client, supporting Sing-box, X-ray, TUIC, Hysteria, Reality, Trojan, SSH etc. It’s an open-source, secure and ad-free."
-  homepage "https://hiddify.com/"
+  homepage "https://github.com/hiddify/hiddify-next"
 
   livecheck do
     url :url
     strategy :github_latest
   end
 
-  container nested: "hiddify-macos-universal.dmg"
-
-  app "Hiddify Next.app"
+  app "Hiddify.app"
 
   zap trash: [
     "~/Library/Application Support/app.hiddify.com",
