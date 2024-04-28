9b29519bb61c65ecb4b3b3141339d6b9ba67f179
ZihanType <ZihanType@protonmail.com>
2024-04-28 20:45:47 +0800

    Add fish-bin 3.7.1

---
+++ fish-bin.rb
@@ -0,0 +1,17 @@
+cask "fish-bin" do
+  version "3.7.1"
+  sha256 "8e45b03fe9fde0e928310a65dcce422ca6d3df12b8417a36de6919cd0fea392f"
+
+  url "https://github.com/fish-shell/fish-shell/releases/download/#{version}/fish-#{version}.app.zip"
+  name "fish-bin"
+  desc "User-friendly command-line shell for UNIX-like operating systems"
+  homepage "https://fishshell.com"
+  license "GPL-2.0-only"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  app "fish.app"
+end
