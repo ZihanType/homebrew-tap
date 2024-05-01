09e3425316533879c4b8351f3ff1ef6944182a1f
ZihanType <ZihanType@protonmail.com>
2024-05-02 00:43:43 +0800

    Update tokei-bin 13.0.0-alpha.0

--- Casks/tokei-bin.rb
+++ Casks/tokei-bin.rb
@@ -1,6 +1,6 @@
 cask "tokei-bin" do
-  version "12.1.2"
-  sha256 "2af8abb6a08b0513f9d16ca2c7cd37949b858d2a3e3227be8cc412b3b4937d5b"
+  version "13.0.0-alpha.0"
+  sha256 "44765d78b981ea88bbfc990af006068e13d19451bed097db1356235a26f93827"
 
   url "https://github.com/XAMPPRocky/tokei/releases/download/v#{version}/tokei-x86_64-apple-darwin.tar.gz"
   name "tokei-bin"
