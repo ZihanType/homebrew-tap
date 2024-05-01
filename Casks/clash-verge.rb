2137a9e2d4866263b84dc7797ab1d43295c7382c
ZihanType <ZihanType@protonmail.com>
2024-05-01 18:24:37 +0800

    Update clash-verge 1.6.1

--- Casks/clash-verge.rb
+++ Casks/clash-verge.rb
@@ -1,6 +1,6 @@
 cask "clash-verge" do
-  version "1.6.0"
-  sha256 "16b4a1693865f61823b0fcf641096ef46b8291987e909f906940835b63e1398c"
+  version "1.6.1"
+  sha256 "40255290e690436acf960b262d7803727f7eec99d269029502c7fd56e5d2baba"
 
   url "https://github.com/clash-verge-rev/clash-verge-rev/releases/download/v#{version}/Clash.Verge_#{version}_x64.dmg"
   name "Clash Verge"
