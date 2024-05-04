6e16c881e1636eb5679ef52eb4f9a4908c7e20c8
ZihanType <ZihanType@protonmail.com>
2024-05-04 09:22:17 +0800

    Update cargo-deny-bin 0.14.23

--- Casks/cargo-deny-bin.rb
+++ Casks/cargo-deny-bin.rb
@@ -1,6 +1,6 @@
 cask "cargo-deny-bin" do
-  version "0.14.22"
-  sha256 "2a753c46a9848229c7fb63be6b00f50a76b132f9a051df7bbcb4717604b8842e"
+  version "0.14.23"
+  sha256 "d8d3e97d95521dcc2e95a4293b3e774aeee5591cad9994ad49b5d177ca98bf8e"
 
   url "https://github.com/EmbarkStudios/cargo-deny/releases/download/#{version}/cargo-deny-#{version}-x86_64-apple-darwin.tar.gz"
   name "cargo-deny-bin"
