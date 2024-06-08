3661fd48b60b6ed104b394a7af870ab444195913
ZihanType <ZihanType@protonmail.com>
2024-06-08 22:20:35 +0800

    Update hiddify-next 1.4.0

--- Casks/hiddify-next.rb
+++ Casks/hiddify-next.rb
@@ -1,6 +1,6 @@
 cask "hiddify-next" do
-  version "1.1.1"
-  sha256 "ef238c7b02955fffdb47421fe371a30f7c12839a17a83c3dee3c3f95a5f67379"
+  version "1.4.0"
+  sha256 "fe7caf7fc0432d9f47a94f25fc04bca99cd4f0e234801dcbb1986df37c1fb85a"
 
   url "https://github.com/hiddify/hiddify-next/releases/download/v#{version}/Hiddify-MacOS.dmg"
   name "Hiddify-Next"
