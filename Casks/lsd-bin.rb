08bb7e53af634f60899110b3983b69c1db2b477a
ZihanType <ZihanType@protonmail.com>
2024-06-17 16:15:09 +0800

    Fix lsd-bin with add `arch`

--- Casks/lsd-bin.rb
+++ Casks/lsd-bin.rb
@@ -1,8 +1,9 @@
 cask "lsd-bin" do
-  version "1.1.2"
-  sha256 "df5cc97fe1c63d3de4e7fda7594743fd961f75024062b1a0c58ca296b8337eb8"
+  arch arm: "aarch64", intel: "x86_64"
 
-  url "https://github.com/lsd-rs/lsd/releases/download/v#{version}/lsd-v#{version}-x86_64-apple-darwin.tar.gz"
+  version "1.1.2"
+
+  url "https://github.com/lsd-rs/lsd/releases/download/v#{version}/lsd-v#{version}-#{arch}-apple-darwin.tar.gz"
   name "lsd-bin"
   desc "The next gen ls command"
   homepage "https://github.com/lsd-rs/lsd"
@@ -12,12 +13,12 @@ cask "lsd-bin" do
     strategy :github_latest
   end
 
-  binary "lsd-v#{version}-x86_64-apple-darwin/lsd"
-  binary "lsd-v#{version}-x86_64-apple-darwin/autocomplete/lsd.bash-completion",
+  binary "lsd-v#{version}-#{arch}-apple-darwin/lsd"
+  binary "lsd-v#{version}-#{arch}-apple-darwin/autocomplete/lsd.bash-completion",
          target: "#{HOMEBREW_PREFIX}/etc/bash_completion.d/lsd"
-  binary "lsd-v#{version}-x86_64-apple-darwin/autocomplete/_lsd",
+  binary "lsd-v#{version}-#{arch}-apple-darwin/autocomplete/_lsd",
          target: "#{HOMEBREW_PREFIX}/share/zsh/site-functions/_lsd"
-  binary "lsd-v#{version}-x86_64-apple-darwin/autocomplete/lsd.fish",
+  binary "lsd-v#{version}-#{arch}-apple-darwin/autocomplete/lsd.fish",
          target: "#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d/lsd.fish"
 
   uninstall delete: [
