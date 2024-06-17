3401a12c05621693bc1dcd8296eb7881b86be880
ZihanType <ZihanType@protonmail.com>
2024-06-17 15:43:16 +0800

    Fix starship-bin with add `arch`

--- Casks/starship-bin.rb
+++ Casks/starship-bin.rb
@@ -1,8 +1,10 @@
 cask "starship-bin" do
+  arch arm: "aarch64", intel: "x86_64"
+
   version "1.19.0"
   sha256 "e29cc00ec9339a8ff85f9903a621311d9d1b6bdac196021594f96e6ab7902b69"
 
-  url "https://github.com/starship/starship/releases/download/v#{version}/starship-x86_64-apple-darwin.tar.gz"
+  url "https://github.com/starship/starship/releases/download/v#{version}/starship-#{arch}-apple-darwin.tar.gz"
   name "starship-bin"
   desc "Cross-shell prompt for astronauts"
   homepage "https://github.com/starship/starship"
