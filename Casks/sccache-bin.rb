4e5bcfdae2ab0e44c55dabf0ccc062a2d297f20a
ZihanType <ZihanType@protonmail.com>
2024-06-17 15:52:36 +0800

    Add sccache-bin with add `arch`

--- Casks/sccache-bin.rb
+++ Casks/sccache-bin.rb
@@ -1,8 +1,9 @@
 cask "sccache-bin" do
-  version "0.8.1"
-  sha256 "4306fb21606b14e2ce1266b25c40e55c090ea68c2cb54e1c39bfb9b81ea3e342"
+  arch arm: "aarch64", intel: "x86_64"
 
-  url "https://github.com/mozilla/sccache/releases/download/v#{version}/sccache-v#{version}-x86_64-apple-darwin.tar.gz"
+  version "0.8.1"
+
+  url "https://github.com/mozilla/sccache/releases/download/v#{version}/sccache-v#{version}-#{arch}-apple-darwin.tar.gz"
   name "sccache-bin"
   desc "sccache is ccache with cloud storage"
   homepage "https://github.com/mozilla/sccache"
@@ -12,5 +13,5 @@ cask "sccache-bin" do
     strategy :github_latest
   end
 
-  binary "sccache-v#{version}-x86_64-apple-darwin/sccache"
+  binary "sccache-v#{version}-#{arch}-apple-darwin/sccache"
 end
