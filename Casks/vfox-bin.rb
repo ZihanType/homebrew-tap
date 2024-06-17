cd0cee4f3332992bf74cbe136c07691fbca7d40a
ZihanType <ZihanType@protonmail.com>
2024-06-17 15:49:48 +0800

    Fix vfox-bin with add `arch`

--- Casks/vfox-bin.rb
+++ Casks/vfox-bin.rb
@@ -1,8 +1,9 @@
 cask "vfox-bin" do
-  version "0.5.3"
-  sha256 "aab755c2b1862b54a78061e7f211fec5c914aa0f661dcd9b3ada5cbd760fa700"
+  arch arm: "aarch64", intel: "x86_64"
 
-  url "https://github.com/version-fox/vfox/releases/download/v#{version}/vfox_#{version}_macos_x86_64.tar.gz"
+  version "0.5.3"
+
+  url "https://github.com/version-fox/vfox/releases/download/v#{version}/vfox_#{version}_macos_#{arch}.tar.gz"
   name "vfox-bin"
   desc "Version manager with support for Java, Node.js, Flutter, .NET & more"
   homepage "https://vfox.lhan.me"
@@ -12,10 +13,10 @@ cask "vfox-bin" do
     strategy :github_latest
   end
 
-  binary "vfox_#{version}_macos_x86_64/vfox"
-  binary "vfox_#{version}_macos_x86_64/completions/bash_autocomplete",
+  binary "vfox_#{version}_macos_#{arch}/vfox"
+  binary "vfox_#{version}_macos_#{arch}/completions/bash_autocomplete",
          target: "#{HOMEBREW_PREFIX}/etc/bash_completion.d/vfox"
-  binary "vfox_#{version}_macos_x86_64/completions/zsh_autocomplete",
+  binary "vfox_#{version}_macos_#{arch}/completions/zsh_autocomplete",
          target: "#{HOMEBREW_PREFIX}/share/zsh/site-functions/_vfox"
 
   uninstall delete: [
