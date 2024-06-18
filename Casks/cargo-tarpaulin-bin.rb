a6c38f9db7b10305aae31970da340c4b4b4f11d7
ZihanType <ZihanType@protonmail.com>
2024-06-18 09:12:21 +0800

    Fix cargo-tarpaulin-bin with add `arch`

--- Casks/cargo-tarpaulin-bin.rb
+++ Casks/cargo-tarpaulin-bin.rb
@@ -1,8 +1,9 @@
 cask "cargo-tarpaulin-bin" do
-  version "0.30.0"
-  sha256 "cc2b8b84c0f19db57f00b5e2e322c3115a744d2fccc823700f3b7f60dd13a289"
+  arch arm: "aarch64", intel: "x86_64"
 
-  url "https://github.com/xd009642/tarpaulin/releases/download/#{version}/cargo-tarpaulin-x86_64-apple-darwin.tar.gz"
+  version "0.30.0"
+
+  url "https://github.com/xd009642/tarpaulin/releases/download/#{version}/cargo-tarpaulin-#{arch}-apple-darwin.tar.gz"
   name "cargo-tarpaulin-bin"
   desc "A code coverage tool for Rust projects"
   homepage "https://github.com/xd009642/tarpaulin"
