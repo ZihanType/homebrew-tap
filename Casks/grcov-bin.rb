671ad21999658e1d8fb2c1bfba2e03e2e09c5250
ZihanType <ZihanType@protonmail.com>
2024-06-18 09:14:09 +0800

    Fix grcov-bin with add `arch`

--- Casks/grcov-bin.rb
+++ Casks/grcov-bin.rb
@@ -1,8 +1,9 @@
 cask "grcov-bin" do
-  version "0.8.19"
-  sha256 "8c4a46740c09ec071ad20eaf916175b8cf7322eacb7c37e905f3d2925690c44a"
+  arch arm: "aarch64", intel: "x86_64"
 
-  url "https://github.com/mozilla/grcov/releases/download/v#{version}/grcov-x86_64-apple-darwin.tar.bz2"
+  version "0.8.19"
+
+  url "https://github.com/mozilla/grcov/releases/download/v#{version}/grcov-#{arch}-apple-darwin.tar.bz2"
   name "grcov-bin"
   desc "Rust tool to collect and aggregate code coverage data for multiple source files"
   homepage "https://github.com/mozilla/grcov"
