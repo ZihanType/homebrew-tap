3dd257f8f619ec3e3e25938f1d9f6c241dc2002e
ZihanType <ZihanType@protonmail.com>
2024-06-30 16:38:19 +0800

    Add jq-bin

---
+++ Casks/jq-bin.rb
@@ -0,0 +1,17 @@
+cask "jq-bin" do
+  arch arm: "arm64", intel: "amd64"
+
+  version "1.7.1"
+
+  url "https://github.com/jqlang/jq/releases/download/jq-#{version}/jq-macos-#{arch}"
+  name "jq-bin"
+  desc "Lightweight and flexible command-line JSON processor"
+  homepage "https://jqlang.github.io/jq/"
+
+  livecheck do
+    url :stable
+    regex(/^(?:jq[._-])?v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  binary "jq-macos-#{arch}", target: "jq"
+end
