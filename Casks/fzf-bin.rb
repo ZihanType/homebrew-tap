253cc767ce450dd515b772148c020515e49f9b49
ZihanType <ZihanType@protonmail.com>
2024-07-08 23:00:53 +0800

    Fix fzf-bin url

--- Casks/fzf-bin.rb
+++ Casks/fzf-bin.rb
@@ -2,8 +2,7 @@ cask "fzf-bin" do
   arch arm: "arm64", intel: "amd64"
 
   version "0.54.0"
-
-  url "https://github.com/junegunn/fzf/releases/download/#{version}/fzf-#{version}-darwin_#{arch}.zip"
+  url "https://github.com/junegunn/fzf/releases/download/v#{version}/fzf-#{version}-darwin_#{arch}.zip"
   name "fzf-bin"
   desc "Command-line fuzzy finder written in Go"
   homepage "https://github.com/junegunn/fzf"
