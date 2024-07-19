5e0675d74d5f8dbe1b993c71dacecb91ccbe3577
ZihanType <ZihanType@protonmail.com>
2024-07-19 20:50:29 +0800

    Update fzf-bin 0.54.1

--- Casks/fzf-bin.rb
+++ Casks/fzf-bin.rb
@@ -1,7 +1,7 @@
 cask "fzf-bin" do
   arch arm: "arm64", intel: "amd64"
 
-  version "0.54.0"
+  version "0.54.1"
   url "https://github.com/junegunn/fzf/releases/download/v#{version}/fzf-#{version}-darwin_#{arch}.zip"
   name "fzf-bin"
   desc "Command-line fuzzy finder written in Go"
