ff2a1c85990bbbf477be6558337cfd46edf90f5a
ZihanType <ZihanType@protonmail.com>
2024-07-23 08:58:43 +0800

    Update cargo-tarpaulin-bin 0.31.0

--- Casks/cargo-tarpaulin-bin.rb
+++ Casks/cargo-tarpaulin-bin.rb
@@ -1,7 +1,7 @@
 cask "cargo-tarpaulin-bin" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.30.0"
+  version "0.31.0"
 
   url "https://github.com/xd009642/tarpaulin/releases/download/#{version}/cargo-tarpaulin-#{arch}-apple-darwin.tar.gz"
   name "cargo-tarpaulin-bin"
