06e1db2636cea0776853e612fca11608aba14084
ZihanType <ZihanType@protonmail.com>
2024-08-07 17:32:25 +0800

    Update regex create-tauri-app-bin

--- Casks/create-tauri-app-bin.rb
+++ Casks/create-tauri-app-bin.rb
@@ -10,9 +10,9 @@ cask "create-tauri-app-bin" do
   homepage "https://github.com/tauri-apps/create-tauri-app"
 
   livecheck do
-    url "https://crates.io/crates/create-tauri-app/versions"
+    url "https://github.com/tauri-apps/create-tauri-app/releases"
     strategy :page_match
-    regex(%r{.*/crates/create-tauri-app/(.+)}i)
+    regex(%r{.*/tauri-apps/create-tauri-app/releases/tag/create-tauri-app-v([.\d]+)}i)
   end
 
   binary "create-tauri-app-#{arch}-apple-darwin", target: "create-tauri-app"
