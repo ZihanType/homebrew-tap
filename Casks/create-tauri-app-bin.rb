786f415f9843bd3ba5943f5e509c14ca9353725c
ZihanType <ZihanType@protonmail.com>
2024-08-07 17:14:14 +0800

    Add create-tauri-app-bin 4.1.0

---
+++ Casks/create-tauri-app-bin.rb
@@ -0,0 +1,19 @@
+cask "create-tauri-app-bin" do
+  arch arm: "aarch64", intel: "x86_64"
+
+  version "4.1.0"
+  sha256 :no_check
+
+  url "https://github.com/tauri-apps/create-tauri-app/releases/download/create-tauri-app-v#{version}/create-tauri-app-#{arch}-apple-darwin"
+  name "create-tauri-app-bin"
+  desc "Rapidly scaffold out a new tauri app project."
+  homepage "https://github.com/tauri-apps/create-tauri-app"
+
+  livecheck do
+    url "https://github.com/tauri-apps/create-tauri-app"
+    strategy :page_match
+    regex(%r{.*/tauri-apps/create-tauri-app/releases/tag/create-tauri-app-v(.+)"}i)
+  end
+
+  binary "create-tauri-app-#{arch}-apple-darwin", target: "create-tauri-app"
+end
