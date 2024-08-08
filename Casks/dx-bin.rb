60d44c901b4fdc65dd2e8a5c47cf43c4a5732b9b
ZihanType <ZihanType@protonmail.com>
2024-08-08 09:48:19 +0800

    Add dx-bin 0.6.0-alpha.2

---
+++ Casks/dx-bin.rb
@@ -0,0 +1,19 @@
+cask "dx-bin" do
+  arch arm: "aarch64", intel: "x86_64"
+
+  version "0.6.0-alpha.2"
+  sha256 :no_check
+
+  url "https://github.com/DioxusLabs/dioxus/releases/download/v#{version}/dx-#{arch}-apple-darwin-v#{version}.tar.gz"
+  name "dx-bin"
+  desc "CLI tool for developing, testing, and publishing Dioxus apps"
+  homepage "https://github.com/DioxusLabs/dioxus"
+
+  livecheck do
+    url "https://github.com/DioxusLabs/dioxus/releases"
+    strategy :page_match
+    regex(%r{.*/DioxusLabs/dioxus/releases/tag/v(.+)" data-view-component="true"}i)
+  end
+
+  binary "dx"
+end
