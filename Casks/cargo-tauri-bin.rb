e1ddaba1bb8eceb56441fec28db42944a2d8106f
ZihanType <ZihanType@protonmail.com>
2024-09-01 10:25:18 +0800

    Upgrade cargo-tauri-bin 2.0.0-rc.9

--- Casks/cargo-tauri-bin.rb
+++ Casks/cargo-tauri-bin.rb
@@ -1,7 +1,7 @@
 cask "cargo-tauri-bin" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "2.0.0-rc.8"
+  version "2.0.0-rc.9"
   sha256 :no_check
 
   url "https://github.com/tauri-apps/tauri/releases/download/tauri-cli-v#{version}/cargo-tauri-#{arch}-apple-darwin.zip"
