dd37c287851f7b9d57b13d0732c14c51a4e3d1fb
ZihanType <ZihanType@protonmail.com>
2024-09-29 18:56:32 +0800

    Upgrade vfox-bin 0.6.0

--- Casks/vfox-bin.rb
+++ Casks/vfox-bin.rb
@@ -1,7 +1,7 @@
 cask "vfox-bin" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.5.5"
+  version "0.6.0"
   sha256 :no_check
 
   url "https://github.com/version-fox/vfox/releases/download/v#{version}/vfox_#{version}_macos_#{arch}.tar.gz"
