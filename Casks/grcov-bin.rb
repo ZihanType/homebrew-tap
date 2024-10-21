74aef43e5c9f680ca663d4330d99653e2ab98ebd
ZihanType <ZihanType@protonmail.com>
2024-10-21 10:42:51 +0800

    Upgrade grcov-bin 0.8.20

--- Casks/grcov-bin.rb
+++ Casks/grcov-bin.rb
@@ -1,7 +1,7 @@
 cask "grcov-bin" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.8.19"
+  version "0.8.20"
   sha256 :no_check
 
   url "https://github.com/mozilla/grcov/releases/download/v#{version}/grcov-#{arch}-apple-darwin.tar.bz2"
