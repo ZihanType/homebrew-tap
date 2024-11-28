f626df21ba6c622a007d5bb6587cba715c1089ee
ZihanType <ZihanType@protonmail.com>
2024-11-28 22:54:22 +0800

    Upgrade cargo-deny-bin 0.16.3

--- Casks/cargo-deny-bin.rb
+++ Casks/cargo-deny-bin.rb
@@ -1,7 +1,7 @@
 cask "cargo-deny-bin" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.16.2"
+  version "0.16.3"
   sha256 :no_check
 
   url "https://github.com/EmbarkStudios/cargo-deny/releases/download/#{version}/cargo-deny-#{version}-#{arch}-apple-darwin.tar.gz"
