f92a71ff900c32f486f6926f25af489aa258bf06
ZihanType <ZihanType@protonmail.com>
2024-12-07 17:50:10 +0800

    Upgrade bindgen-bin 0.71.0

--- Casks/bindgen-bin.rb
+++ Casks/bindgen-bin.rb
@@ -1,7 +1,7 @@
 cask "bindgen-bin" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.70.1"
+  version "0.71.0"
   sha256 :no_check
 
   url "https://github.com/rust-lang/rust-bindgen/releases/download/v#{version}/bindgen-cli-#{arch}-apple-darwin.tar.xz"
