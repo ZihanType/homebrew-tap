61fb7e1eb321edfe104945481dd33ba4af1ae2f2
ZihanType <ZihanType@protonmail.com>
2025-01-08 09:31:55 +0800

    Upgrade bat-bin 0.25.0

--- Casks/bat-bin.rb
+++ Casks/bat-bin.rb
@@ -1,8 +1,10 @@
 cask "bat-bin" do
-  version "0.24.0"
+  arch arm: "aarch64", intel: "x86_64"
+
+  version "0.25.0"
   sha256 :no_check
 
-  url "https://github.com/sharkdp/bat/releases/download/v#{version}/bat-v#{version}-x86_64-apple-darwin.tar.gz"
+  url "https://github.com/sharkdp/bat/releases/download/v#{version}/bat-v#{version}-#{arch}-apple-darwin.tar.gz"
   name "bat-bin"
   desc "A cat(1) clone with wings."
   homepage "https://github.com/sharkdp/bat"
@@ -12,11 +14,11 @@ cask "bat-bin" do
     strategy :github_latest
   end
 
-  binary "bat-v#{version}-x86_64-apple-darwin/bat"
-  binary "bat-v#{version}-x86_64-apple-darwin/autocomplete/bat.bash",
+  binary "bat-v#{version}-#{arch}-apple-darwin/bat"
+  binary "bat-v#{version}-#{arch}-apple-darwin/autocomplete/bat.bash",
          target: "#{HOMEBREW_PREFIX}/etc/bash_completion.d/bat"
-  binary "bat-v#{version}-x86_64-apple-darwin/autocomplete/bat.zsh",
+  binary "bat-v#{version}-#{arch}-apple-darwin/autocomplete/bat.zsh",
          target: "#{HOMEBREW_PREFIX}/share/zsh/site-functions/_bat"
-  binary "bat-v#{version}-x86_64-apple-darwin/autocomplete/bat.fish",
+  binary "bat-v#{version}-#{arch}-apple-darwin/autocomplete/bat.fish",
          target: "#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d/bat.fish"
 end
