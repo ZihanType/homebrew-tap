0a916ec5f29325db9d3fc1ef21775010bcc6fe66
ZihanType <ZihanType@protonmail.com>
2025-02-05 15:08:40 +0800

    Add goose-bin

---
+++ Casks/goose-bin.rb
@@ -0,0 +1,18 @@
+cask "goose-bin" do
+  arch arm: "aarch64", intel: "x86_64"
+
+  version "1.0.4"
+  sha256 :no_check
+
+  url "https://github.com/block/goose/releases/download/v#{version}/goose-#{arch}-apple-darwin.tar.bz2"
+  name "goose-bin"
+  desc "an open-source, extensible AI agent that goes beyond code suggestions - install, execute, edit, and test with any LLM"
+  homepage "https://github.com/block/goose"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  binary "goose"
+end
