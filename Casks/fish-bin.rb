b5c410c529046d0537364912535ba6fd632c2ff3
ZihanType <ZihanType@protonmail.com>
2025-03-13 15:28:03 +0800

    Fix app original name

--- Casks/fish-bin.rb
+++ Casks/fish-bin.rb
@@ -14,7 +14,8 @@ cask "fish-bin" do
 
   conflicts_with formula: "fish"
 
-  app "fish-#{version}.app", target: "fish.app"
+#   app "fish-#{version}.app", target: "fish.app"
+  app "fish-67b6aff.app", target: "fish.app"
   binary "#{appdir}/fish.app/Contents/Resources/base/usr/local/bin/fish",
          target: "/usr/local/bin/fish"
   binary "#{appdir}/fish.app/Contents/Resources/base/usr/local/bin/fish_indent",
