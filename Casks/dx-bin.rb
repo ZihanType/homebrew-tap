1c114cbde85a5b685c839e5c44b05e5b1eeeed09
ZihanType <ZihanType@protonmail.com>
2025-03-15 15:56:29 +0800

    Upgrade dx-bin 0.6.3

--- Casks/dx-bin.rb
+++ Casks/dx-bin.rb
@@ -1,7 +1,7 @@
 cask "dx-bin" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.6.0-alpha.2"
+  version "0.6.3"
   sha256 :no_check
 
   url "https://github.com/DioxusLabs/dioxus/releases/download/v#{version}/dx-#{arch}-apple-darwin-v#{version}.tar.gz"
