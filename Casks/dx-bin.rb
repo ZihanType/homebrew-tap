dc490c6f2dba8b865ac149d39f87e827569d53ba
ZihanType <ZihanType@protonmail.com>
2025-05-16 14:54:52 +0800

    Downgrade dx-bin 0.6.3

--- Casks/dx-bin.rb
+++ Casks/dx-bin.rb
@@ -1,18 +1,17 @@
 cask "dx-bin" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.7.0-alpha.0"
+  version "0.6.3"
   sha256 :no_check
 
-  url "https://github.com/DioxusLabs/dioxus/releases/download/v#{version}/dx-#{arch}-apple-darwin.tar.gz"
+  url "https://github.com/DioxusLabs/dioxus/releases/download/v#{version}/dx-#{arch}-apple-darwin-v#{version}.tar.gz"
   name "dx-bin"
   desc "CLI tool for developing, testing, and publishing Dioxus apps"
   homepage "https://github.com/DioxusLabs/dioxus"
 
   livecheck do
-    url "https://github.com/DioxusLabs/dioxus/releases"
-    strategy :page_match
-    regex(%r{.*/DioxusLabs/dioxus/releases/tag/v(.+)" data-view-component="true"}i)
+    url "https://crates.io/api/v1/crates/dioxus-cli"
+    regex(%r{.*"max_stable_version":"(.+?)"}i)
   end
 
   binary "dx"
