2099ce04bdcd62761c6417b67fb29bcb409c38e8
ZihanType <ZihanType@protonmail.com>
2025-05-20 15:25:49 +0800

    Upgrade cargo-tarpaulin-bin 0.32.6

--- Casks/cargo-tarpaulin-bin.rb
+++ Casks/cargo-tarpaulin-bin.rb
@@ -1,7 +1,7 @@
 cask "cargo-tarpaulin-bin" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.32.5"
+  version "0.32.6"
   sha256 :no_check
 
   url "https://github.com/xd009642/tarpaulin/releases/download/#{version}/cargo-tarpaulin-#{arch}-apple-darwin.tar.gz"
