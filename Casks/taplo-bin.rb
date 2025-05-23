82f9ad0c5b9679f99ce2ae82f8a94be94c75f123
ZihanType <ZihanType@protonmail.com>
2025-05-23 12:38:29 +0800

    Fix taplo-bin url

--- Casks/taplo-bin.rb
+++ Casks/taplo-bin.rb
@@ -4,7 +4,7 @@ cask "taplo-bin" do
   version "0.10.0"
   sha256 :no_check
 
-  url "https://github.com/tamasfe/taplo/releases/download/#{version}/taplo-full-darwin-#{arch}.gz"
+  url "https://github.com/tamasfe/taplo/releases/download/#{version}/taplo-darwin-#{arch}.gz"
   name "taplo-bin"
   desc "TOML toolkit written in Rust"
   homepage "https://github.com/tamasfe/taplo"
