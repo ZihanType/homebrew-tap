aaf01131124f913c80ed2584513a0fff1b32cb5d
ZihanType <ZihanType@protonmail.com>
2025-06-01 20:53:05 +0800

    Upgrade dx-bin 0.7.0-alpha.1

--- Casks/dx-bin.rb
+++ Casks/dx-bin.rb
@@ -1,18 +1,18 @@
 cask "dx-bin" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.6.3"
+  version "0.7.0-alpha.1"
   sha256 :no_check
 
-  url "https://github.com/DioxusLabs/dioxus/releases/download/v#{version}/dx-#{arch}-apple-darwin-v#{version}.tar.gz"
+  url "https://github.com/DioxusLabs/dioxus/releases/download/v#{version}/dx-#{arch}-apple-darwin.tar.gz"
   name "dx-bin"
   desc "CLI tool for developing, testing, and publishing Dioxus apps"
   homepage "https://github.com/DioxusLabs/dioxus"
 
-  livecheck do
-    url "https://crates.io/api/v1/crates/dioxus-cli"
-    regex(%r{.*"max_stable_version":"(.+?)"}i)
-  end
+#   livecheck do
+#     url "https://crates.io/api/v1/crates/dioxus-cli"
+#     regex(%r{.*"max_stable_version":"(.+?)"}i)
+#   end
 
   binary "dx"
 end
