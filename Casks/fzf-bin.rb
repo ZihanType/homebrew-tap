1148e032814812df994e658bf9e546049d907a27
ZihanType <ZihanType@protonmail.com>
2025-06-28 01:16:52 +0800

    Upgrade fzf-bin 0.63.0

--- Casks/fzf-bin.rb
+++ Casks/fzf-bin.rb
@@ -1,7 +1,7 @@
 cask "fzf-bin" do
   arch arm: "arm64", intel: "amd64"
 
-  version "0.62.0"
+  version "0.63.0"
   sha256 :no_check
 
   url "https://github.com/junegunn/fzf/releases/download/v#{version}/fzf-#{version}-darwin_#{arch}.tar.gz"
