6908cc3a7231b90f8b4a255dc224f511cb6fd6d2
ZihanType <ZihanType@protonmail.com>
2025-07-24 19:50:27 +0800

    Upgrade create-tauri-app-bin 4.6.1

--- Casks/create-tauri-app-bin.rb
+++ Casks/create-tauri-app-bin.rb
@@ -1,10 +1,11 @@
 cask "create-tauri-app-bin" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "4.5.10"
+  version "4.6.1"
   sha256 :no_check
 
-  url "https://github.com/tauri-apps/create-tauri-app/releases/download/create-tauri-app-v#{version}/create-tauri-app-#{arch}-apple-darwin"
+#   url "https://github.com/tauri-apps/create-tauri-app/releases/download/create-tauri-app-v#{version}/create-tauri-app-#{arch}-apple-darwin"
+  url "https://github.com/ZihanType/homebrew-tap/releases/download/create-tauri-app-v#{version}/create-tauri-app-#{arch}-apple-darwin"
   name "create-tauri-app-bin"
   desc "Rapidly scaffold out a new tauri app project."
   homepage "https://github.com/tauri-apps/create-tauri-app"
