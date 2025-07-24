4b5995aefea2b8ab21f6537327cfdc40410aa913
ZihanType <ZihanType@protonmail.com>
2025-07-24 20:08:12 +0800

    Upgrade bindgen-bin 0.72.0

--- Casks/bindgen-bin.rb
+++ Casks/bindgen-bin.rb
@@ -1,10 +1,11 @@
 cask "bindgen-bin" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.71.1"
+  version "0.72.0"
   sha256 :no_check
 
-  url "https://github.com/rust-lang/rust-bindgen/releases/download/v#{version}/bindgen-cli-#{arch}-apple-darwin.tar.xz"
+#   url "https://github.com/rust-lang/rust-bindgen/releases/download/v#{version}/bindgen-cli-#{arch}-apple-darwin.tar.xz"
+  url "https://github.com/ZihanType/homebrew-tap/releases/download/v#{version}/bindgen-cli-#{arch}-apple-darwin.tar.xz"
   name "bindgen-bin"
   desc "Automatically generates Rust FFI bindings to C (and some C++) libraries"
   homepage "https://github.com/rust-lang/rust-bindgen"
