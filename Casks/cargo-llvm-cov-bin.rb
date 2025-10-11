f87b2c28d9097c8dbf33ad715238430c21072230
ZihanType <ZihanType@protonmail.com>
2025-10-12 01:27:07 +0800

    Add cargo-llvm-cov-bin

---
+++ Casks/cargo-llvm-cov-bin.rb
@@ -0,0 +1,18 @@
+cask "cargo-llvm-cov-bin" do
+  arch arm: "aarch64", intel: "x86_64"
+
+  version "0.6.21"
+  sha256 :no_check
+
+  url "https://github.com/taiki-e/cargo-llvm-cov/releases/download/v#{version}/cargo-llvm-cov-#{arch}-apple-darwin.tar.gz"
+  name "cargo-llvm-cov-bin"
+  desc "Cargo subcommand to easily use LLVM source-based code coverage"
+  homepage "https://github.com/taiki-e/cargo-llvm-cov"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  binary "cargo-llvm-cov"
+end
