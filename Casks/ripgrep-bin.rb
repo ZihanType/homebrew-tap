50257eeee5ef76235df8bb2d73561ddd1846de51
ZihanType <ZihanType@protonmail.com>
2025-10-22 21:55:41 +0800

    Upgrade ripgrep-bin 15.1.0

--- Casks/ripgrep-bin.rb
+++ Casks/ripgrep-bin.rb
@@ -1,7 +1,7 @@
 cask "ripgrep-bin" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "15.0.0"
+  version "15.1.0"
   sha256 :no_check
 
   url "https://github.com/BurntSushi/ripgrep/releases/download/#{version}/ripgrep-#{version}-#{arch}-apple-darwin.tar.gz"
