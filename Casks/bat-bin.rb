234b9369546e063b00c215eb3918f58461a4966a
ZihanType <ZihanType@protonmail.com>
2025-12-03 17:28:00 +0800

    Upgrade bat-bin 0.26.1

--- Casks/bat-bin.rb
+++ Casks/bat-bin.rb
@@ -1,7 +1,7 @@
 cask "bat-bin" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.26.0"
+  version "0.26.1"
   sha256 :no_check
 
   url "https://github.com/sharkdp/bat/releases/download/v#{version}/bat-v#{version}-#{arch}-apple-darwin.tar.gz"
