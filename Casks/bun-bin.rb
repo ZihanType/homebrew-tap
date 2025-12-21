a4bab116e8a78975704596777634e69aea87f143
ZihanType <ZihanType@protonmail.com>
2025-12-21 22:08:45 +0800

    Add bun-bin

---
+++ Casks/bun-bin.rb
@@ -0,0 +1,20 @@
+cask "bun-bin" do
+  arch arm: "aarch64", intel: "x64"
+
+  version "1.3.5"
+  sha256 :no_check
+
+  url "https://github.com/oven-sh/bun/releases/download/bun-v#{version}/bun-darwin-#{arch}.zip"
+  name "bun-bin"
+  desc "Incredibly fast JavaScript runtime, bundler, transpiler and package manager - all in one."
+  homepage "https://bun.sh/"
+
+  livecheck do
+    url "https://github.com/oven-sh/bun/releases/latest"
+    regex(%r{href=.*?/tag/bun-v?(\d+(?:\.\d+)+)["' >]}i)
+  end
+
+  binary "bun"
+  binary "bun", target: "bunx"
+  binary "bun", target: "node"
+end
