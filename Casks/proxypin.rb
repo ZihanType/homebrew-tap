26910cc878c0bb6adf4d09c90e4b2a437d798efa
ZihanType <ZihanType@protonmail.com>
2026-06-27 10:18:04 +0800

    add proxypin

---
+++ Casks/proxypin.rb
@@ -0,0 +1,24 @@
+cask "proxypin" do
+  version "1.2.9"
+  sha256 :no_check
+
+  url "https://github.com/wanghongenpin/proxypin/releases/download/v#{version}/proxypin-macos.dmg"
+  name "ProxyPin"
+  desc "Open source free capture HTTP(S) traffic software ProxyPin, supporting full platform systems"
+  homepage "https://github.com/wanghongenpin/proxypin"
+
+  depends_on macos: :ventura
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "ProxyPin.app"
+
+  zap trash: [
+    # "~/Library/Application Support/Snapzy",
+    # "~/Library/Preferences/Snapzy.plist",
+    # "~/Library/Caches/Snapzy",
+  ]
+end
