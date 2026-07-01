eaacb5c9a3ca0272979d4953d4f617eb824b24ad
ZihanType <ZihanType@protonmail.com>
2026-07-01 18:26:34 +0800

    add lark-cli-bin

---
+++ Casks/lark-cli-bin.rb
@@ -0,0 +1,18 @@
+cask "lark-cli-bin" do
+  arch arm: "arm64", intel: "amd64"
+
+  version "1.0.61"
+  sha256 :no_check
+
+  url "https://github.com/larksuite/cli/releases/download/v#{version}/lark-cli-#{version}-darwin-#{arch}.tar.gz"
+  name "lark-cli-bin"
+  desc "The official Lark/Feishu CLI tool."
+  homepage "https://github.com/larksuite/cli"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  binary "lark-cli"
+end
