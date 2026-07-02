7ec30d2b0ab032c9635962415e9d52a7d7c649d9
ZihanType <ZihanType@protonmail.com>
2026-07-02 16:41:47 +0800

    add kache-bin

---
+++ Casks/kache-bin.rb
@@ -0,0 +1,18 @@
+cask "kache-bin" do
+  arch arm: "aarch64", intel: "x86_64"
+
+  version "0.8.0"
+  sha256 :no_check
+
+  url "https://github.com/kunobi-ninja/kache/releases/download/v#{version}/kache-#{arch}-apple-darwin.tar.gz"
+  name "kache-bin"
+  desc "Zero-copy, content-addressed Rust build cache for Rust, C/C++ and more."
+  homepage "https://github.com/kunobi-ninja/kache"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  binary "kache"
+end
